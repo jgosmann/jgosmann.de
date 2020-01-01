07e999cae2cac75daeed4a33b5b9e702a1be7731
Jan Gosmann <jan@hyper-world.de>
2020-01-01 12:30:51 +0100

    Change webpack.config.json indentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    output: {
-        filename: 'bundle.js'
-    },
+  mode: 'production',
+  output: {
+    filename: 'bundle.js'
+  },
 
-    devtool: "source-map",
-    resolve: {
-        extensions: [".ts", ".js", ".json"]
-    },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".js", ".json"]
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: "awesome-typescript-loader",
-                exclude: '/node_modules/'
-            }, {
-                enforce: "pre",
-                test: /\.js$/,
-                loader: "source-map-loader"
-            }
-        ]
-    }
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "awesome-typescript-loader",
+        exclude: '/node_modules/'
+      }, {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  }
 };
