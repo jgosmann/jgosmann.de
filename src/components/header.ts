2396a6e8f85c5692c0f4547786396a10d6e33f51
Jan Gosmann <jan@hyper-world.de>
2023-05-01 20:03:08 +0200

    Format with prettier

--- src/components/header.ts
+++ src/components/header.ts
@@ -2,6 +2,6 @@ export const headerLargeHeight = 128;
 export const verticalHeaderMinWidth = 600;
 
 export const headerCssVars = {
-    'header-large-height': `${headerLargeHeight}px`,
-    'vertical-header-min-width': `${verticalHeaderMinWidth}px`,
-}
+  "header-large-height": `${headerLargeHeight}px`,
+  "vertical-header-min-width": `${verticalHeaderMinWidth}px`,
+};
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,12 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 export const collections = {
-    'about': defineCollection({
-        schema: ({ image }) => z.object({
-            background: z.optional(image()),
-            leadTitle: z.string(),
-            title: z.string(),
-        })
-    })
-}
+  about: defineCollection({
+    schema: ({ image }) =>
+      z.object({
+        background: z.optional(image()),
+        leadTitle: z.string(),
+        title: z.string(),
+      }),
+  }),
+};
