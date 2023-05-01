8dab0019c1b9c5d00775862627c92e74f69054b4
Jan Gosmann <jan@hyper-world.de>
2023-05-01 20:01:54 +0200

    Fix header layout/style issues

--- src/components/header.ts
+++ src/components/header.ts
@@ -1,1 +1,7 @@
 export const headerLargeHeight = 128;
+export const verticalHeaderMinWidth = 600;
+
+export const headerCssVars = {
+    'header-large-height': `${headerLargeHeight}px`,
+    'vertical-header-min-width': `${verticalHeaderMinWidth}px`,
+}
