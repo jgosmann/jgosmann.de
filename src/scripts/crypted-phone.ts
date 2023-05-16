5607d7ebf46cedc2d71a7e87d847a65780a930b4
Jan Gosmann <jan@hyper-world.de>
2023-05-16 21:03:25 +0200

    Fix phone link prefix

--- src/scripts/crypted-phone.ts
+++ src/scripts/crypted-phone.ts
@@ -9,7 +9,7 @@ class CryptedPhone extends HTMLAnchorElement {
     const block0 = this.getAttribute("block0");
     const block1 = this.getAttribute("block1");
 
-    this.href = `phone:${country}${area}${block0}${block1}`;
+    this.href = `tel:${country}${area}${block0}${block1}`;
     this.textContent = `${country}${narrowNbsp}${area}${narrowNbsp}${block0}${narrowNbsp}${block1}`;
   }
 }
